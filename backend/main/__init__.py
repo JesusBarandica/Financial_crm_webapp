30fbea1a0579dba8eee45087e3f855efc9120924
unknown <jesusbarandicaromero@gmail.com>
2023-02-25 12:30:49 -0500

    commit 2 creacion y configuracion base de datos sin modelos en nuestra aplicación para despliegue local

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -19,6 +19,28 @@ def create_app():
     load_dotenv()
 
 
+    #configuración base de datos
+
+    ###traemos variables de entorno y las instanciamos
+    PATH = os.getenv("PATH_DATABASE")
+    NAME_DB = os.getenv("NAME_DATABASE")
+    ### creamos un condicional para verificar si la base de datos existen en esta ruta.
+    ### utilizamos os.path.exits para verificar si ruta existe
+    if not os.path.exists(f"{PATH}{NAME_DB}"):
+    ### si la ruta no existe nos dirigimos a la ruta
+        os.chdir(f"{PATH}")
+    ### creamos base de datos con el sistema operativo
+        file = os.open(f"{NAME_DB}", os.O_CREAT )
+
+    ###ya con nuestra base de datos creada, configuramos nuestra aplicación
+    ###desahabilitamos los commit automaticos para nuestra aplicación
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    ###habilitamos para que nuestra aplicación se conecte a nuestra db en la ruta especificada.
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{PATH}{NAME_DB}"
+    ### inicializamos las configuración que hicimos a nuestra base de datos y las aplicamos
+    db.init_app(app)
+
+    
     return app
 
 
--- backend/main/resources/usuarios.py
+++ backend/main/resources/usuarios.py
@@ -3,9 +3,11 @@ from flask_restful import Resource
 ###importamos de flask jsonify para serializar objetos a formato json y request para poder recibir las peticiones del cliente
 from flask import jsonify, request
 
-
+###Creamos los recursos de la subclase Usuarios la cual es heredada de recursos, para manipular la base de datos, de los
+###roles que se crearan en el sistema de manera masiva.
 class Usuarios(Resource):
     pass
 
+##Creamos los recursos de la subclase Usuarios la cual es heredada de recursos, para manipular datos de usuario de manera individual
 class Usuario(Resource):
     pass
